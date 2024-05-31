afe86bd84ccaa2782ce84fbf91b212b0f5f771dc
Ahmed Yasser <ahmedyasser3033@gmail.com>
2024-06-01 01:59:14 +0200

    installed necessary dependencies and added sidebar

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Logo = () => {
+  return (
+   <Image 
+   
+    width = {130}
+    height={130}
+    src={'/logo.svg'}
+    alt = "logo"
+   />
+  )
+}
+
+export default Logo
---
+++ middleware.ts
@@ -0,0 +1,12 @@
+
+
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: ["/api/webhook", "/api/uploadthing" ]
+});
+ 
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+ 
